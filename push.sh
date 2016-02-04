07aae308fd3afb1e05dced149dd2ea101ea74af6
mpenchev86 <peace.glory@yahoo.com>
2016-02-04 21:48:24 +0200

    no

--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# cd d/TAcademy/Track WEB/ASP.NET-MVC/Final Project/ASP.NET-MVC-FinalProject
-git clean -f -X
-git add .
-echo "Commit description: "
-read -e desc
-git commit -m "$desc"
-git push
